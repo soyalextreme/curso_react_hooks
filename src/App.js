ad27898b8f4acbe22fbf6095ba9774624b629c80
soyalextreme <soyalextreme@gmail.com>
2021-05-29 23:58:14 -0500

    useContext

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import './App.css';
 import Header from "./components/Header";
 import Characters from "./components/Characters";
 
+// context
+import ThemeContext from "./context/ThemeContext"
+
 function App() {
 const [darkMode, setDarkMode] = useState(false);
 
   
   return (
+    <ThemeContext.Provider value="green">
     <div className="App">
       <Header dark={{val: darkMode, set: setDarkMode}}/>
       <Characters dark={darkMode} />
     </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import ThemeContext from "../context/ThemeContext";
 
 const Header = ({ dark }) => {
+  const color = useContext(ThemeContext);
+
   const hanldeClick = () => {
     dark.set(!dark.val);
   };
@@ -8,16 +11,16 @@ const Header = ({ dark }) => {
   return (
     <div
       className="Header"
-       style={{
-          backgroundColor: dark.val ? "black" : "white",
-          color: dark.val ? "white" : "black",
-          padding: "2rem",
-          borderBottomWidth: "0.3rem",
-          borderBottomColor: "rgba(171, 224, 96, 0.767)",
-          borderBottomStyle: "solid"
-        }}
+      style={{
+        backgroundColor: dark.val ? "black" : "white",
+        color: dark.val ? "white" : "black",
+        padding: "2rem",
+        borderBottomWidth: "0.3rem",
+        borderBottomColor: "rgba(171, 224, 96, 0.767)",
+        borderBottomStyle: "solid",
+      }}
     >
-      <h1>React Hooks</h1>
+      <h1 style={{    color    }}>React Hooks</h1>
       <button type="button" onClick={hanldeClick}>
         {dark.val ? "Dark Mode" : "Light mode"}
       </button>
